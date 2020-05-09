aea411ebddbb14ff2d66478dfb641f287bbdab68
yaraelg <yara.elg95@gmail.com>
2020-05-09 12:26:48 +0200

    he cambiado la ultima linea

---
+++ main.js
@@ -0,0 +1,26 @@
+var a = "Amigo";
+alert(a.toLowerCase()); // 'amigo'
+
+var a = "Amigo";
+alert(a.replace(“Am”, “am”));
+
+var myStr = "Amigo";
+myStr = "amigo";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
